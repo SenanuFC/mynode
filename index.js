79e0d130e6ae6ba0381915a22997885734778b43
SenanuFC <senanufc@blankiegh.com>
2016-06-29 17:17:10 +0000

    Example of SOAP Call using weather service
    Nodejs SOAP call to worldweathers service example

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var port = 3000;
 var app = express();
+var soap = require('soap');
+var xml2js = require('xml2js');
+// var prompt = require('prompt');
+// var parser = require('xml2js').parseString;
+
+
+//EXAMPLE
+var args1 = {"CityName": "Accra", "CountryName":"Ghana"};
+var url1 = "http://www.webservicex.com/globalweather.asmx?wsdl";
+//XML Parser
+var parser = new xml2js.Parser();
+soap.createClient(url1, function(err, client) {
+      client.GetWeather(args1, function(err, result) {
+      	parser.parseString(result.GetWeatherResult, function(err, result){
+      		var json = JSON.stringify(result);
+      		console.log(json);
+      	})
+      });
+});
+
+
+//AAA SOAP CALLS
+//
 
 
 app.use(function(req, res, next){
